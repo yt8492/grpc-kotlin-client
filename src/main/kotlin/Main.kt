dc7f007fc7c33738dc25131dcefa5f06e4685e60
yt8492 <tmymyut2000@gmail.com>
2020-03-03 23:21:19 +0900

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,66 @@
+import com.yt8492.grpcsample.protobuf.*
+import io.grpc.ManagedChannelBuilder
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.runBlocking
+
+@ExperimentalCoroutinesApi
+fun main() {
+    val channel = ManagedChannelBuilder.forAddress("localhost", 6565)
+        .usePlaintext()
+        .build()
+    val client = MessageServiceGrpc.newStub(channel)
+    runBlocking {
+        println("--- Unary Call start ---")
+        val request = MessageRequest.newBuilder()
+            .setMessage("hoge")
+            .build()
+        val response = client.unary(request)
+        println(response.message)
+        println("--- Unary Call finish ---")
+    }
+    runBlocking {
+        println("--- Client Stream start ---")
+        val call = client.clientStream()
+        listOf("hoge", "fuga", "piyo")
+            .map {
+                MessageRequest.newBuilder()
+                    .setMessage(it)
+                    .build()
+            }.forEach {
+                call.send(it)
+            }
+        call.close()
+        val response = call.await()
+        println(response.message)
+        println("--- Client Stream finish ---")
+    }
+    runBlocking {
+        println("--- Server Stream start ---")
+        val request = MessageRequest.newBuilder()
+            .setMessage("hoge")
+            .build()
+        val call = client.serverStream(request)
+        call.consumeEach {
+            println(it.message)
+        }
+        println("--- Server Stream finish ---")
+    }
+    runBlocking {
+        println("--- Bidirectional Stream start ---")
+        val call = client.bidirectionalStream()
+        listOf("hoge", "fuga", "piyo")
+            .map {
+                MessageRequest.newBuilder()
+                    .setMessage(it)
+                    .build()
+            }.forEach {
+                call.send(it)
+            }
+        call.close()
+        call.consumeEach {
+            println(it.message)
+        }
+        println("--- Bidirectional Stream finish ---")
+    }
+}
